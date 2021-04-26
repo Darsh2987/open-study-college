94c022cae1b900439522d10d075be49bc7b6f583
Darsh <darsh2987@gmail.com>
2021-04-26 14:18:18 +0100

    initial commit - webpack build, starting html, scss and js files

---
+++ src/assets/js/app.js
@@ -0,0 +1,5 @@
+import "../scss/imports.scss";
+
+window.addEventListener("load", () => {
+  alert("JavaScript is working!!!");
+});
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/assets/js/app.js",
+  output: {
+    filename: "bundled-script.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundled-styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "index",
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/i,
+        use: [MiniCssExtractPlugin.loader, { loader: "css-loader" }, { loader: "sass-loader" }],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./images",
+              publicPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./fonts",
+              publicPath: "fonts",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
