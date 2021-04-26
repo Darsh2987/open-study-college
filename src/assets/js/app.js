6ec78a1958d59d5cf4b3ecade24bef49c8fa8043
Darsh <darsh2987@gmail.com>
2021-04-26 18:14:50 +0100

    functions to call api data, randomise and display to DOM

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,35 @@
 import "../scss/imports.scss";
+import Axios from "axios";
 
 window.addEventListener("load", () => {
-  alert("JavaScript is working!!!");
+  const spinner = document.querySelector("#spinner");
+  const cardData = document.querySelector("#data");
+
+  function outputData(outputdata) {
+    console.log(outputdata);
+    cardData.innerHTML = `${outputdata.id} ${outputdata.name} ${outputdata.telephone} ${outputdata.email} ${outputdata.status} ${outputdata.course_title ? outputdata.course_title : "No Course's"}`;
+  }
+
+  function randomDataItem(data) {
+    const random = Math.floor(Math.random() * data.length);
+    outputData(data[random]);
+  }
+
+  async function fetchData() {
+    const image = document.createElement("img");
+    image.src = "./images/spinner.gif";
+    spinner.appendChild(image);
+
+    try {
+      const response = await Axios("http://leads.beta.openstudycollege.info/getTopLeads");
+      const data = response.data;
+      spinner.removeChild(image);
+      randomDataItem(data);
+      cardData.classList.add("data--visible");
+    } catch (e) {
+      console.log("ERROR - There was a error fetching the data");
+    }
+  }
+
+  fetchData();
 });
