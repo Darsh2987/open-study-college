fce0d02e7240e6ac80efd3e2cd34c335687220e9
Darsh <darsh2987@gmail.com>
2021-04-26 19:27:06 +0100

    initial comments

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
 import "../scss/imports.scss";
 import Axios from "axios";
 
+//Window Load Function
 window.addEventListener("load", () => {
   const spinner = document.querySelector("#spinner");
   const cardData = document.querySelector("#data");
@@ -10,16 +11,20 @@ window.addEventListener("load", () => {
     cardData.innerHTML = `${outputdata.id} ${outputdata.name} ${outputdata.telephone} ${outputdata.email} ${outputdata.status} ${outputdata.course_title ? outputdata.course_title : "No Course's"}`;
   }
 
+  // Function Random - give us a randomw item from the API array of data
   function randomDataItem(data) {
     const random = Math.floor(Math.random() * data.length);
     outputData(data[random]);
   }
 
+  // Async Function to Fetch Data from API
   async function fetchData() {
+    // Creating the Spinner Element - to show when waiting for the API call to complete
     const image = document.createElement("img");
     image.src = "./images/spinner.gif";
     spinner.appendChild(image);
 
+    // Try Block - API Call, Remove Spinner, Pass API Data into "randomDataItem" Function
     try {
       const response = await Axios("http://leads.beta.openstudycollege.info/getTopLeads");
       const data = response.data;
@@ -31,5 +36,6 @@ window.addEventListener("load", () => {
     }
   }
 
+  // Call FetchData Function
   fetchData();
 });
